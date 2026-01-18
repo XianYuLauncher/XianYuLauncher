205fb6cd3aea1e879b21c5c5eac38c73af95e175
14231 <14231@SpiritStudio>
2026-01-18 23:07:20 +0800

    fix:后台下载版本 不显示版本自定义名称

--- XianYuLauncher/ViewModels/ShellViewModel.cs
+++ XianYuLauncher/ViewModels/ShellViewModel.cs
@@ -68,14 +68,16 @@ public partial class ShellViewModel : ObservableRecipient
             switch (taskInfo.State)
             {
                 case DownloadTaskState.Downloading:
+                    // 始终更新任务信息，但只有 IsTeachingTipEnabled 为 true 时才打开 TeachingTip
+                    DownloadTaskName = taskInfo.TaskName;
+                    DownloadStatusMessage = taskInfo.StatusMessage;
+                    DownloadProgress = taskInfo.Progress;
+                    
                     // 只有当 IsTeachingTipEnabled 为 true 时才打开 TeachingTip
                     // 这样可以避免在下载弹窗打开时同时打开 TeachingTip
                     // 用户点击"后台下载"按钮时会设置 IsTeachingTipEnabled = true
                     if (_downloadTaskManager.IsTeachingTipEnabled)
                     {
-                        DownloadTaskName = taskInfo.TaskName;
-                        DownloadStatusMessage = taskInfo.StatusMessage;
-                        DownloadProgress = taskInfo.Progress;
                         IsDownloadTeachingTipOpen = true;
                     }
                     break;
